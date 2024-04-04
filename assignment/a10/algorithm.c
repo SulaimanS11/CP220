912ba00430fd83f771b19b3167b5ada9445f94d8
SulaimanS <mirsulaimansultan@gmail.com>
2024-04-04 00:00:15 -0400

    a10 submit

--- assignment/a10/algorithm.c
+++ assignment/a10/algorithm.c
@@ -13,14 +13,48 @@
 #include <stdlib.h> 
 #include "heap.h"
 #include "algorithm.h"
+#include <limits.h>
+#include <stdbool.h>
 
 EDGELIST *mst_prim(GRAPH *g, int start) {
 // your code
 }
 
 EDGELIST *spt_dijkstra(GRAPH *g, int start) {
-// your code
+    int numVertices = g->order;
+    int dist[numVertices];
+    bool sptSet[numVertices];
+
+    for (int i = 0; i < numVertices; i++) {
+        dist[i] = INT_MAX, sptSet[i] = false;
+    }
+
+    dist[start] = 0;
+
+
+    for (int count = 0; count < numVertices - 1; count++) {
+
+        int u = -1;
+        int min = INT_MAX;
+
+        for (int v = 0; v < numVertices; v++) {
+            if (sptSet[v] == false && dist[v] <= min) {
+                min = dist[v], u = v;
+            }
+        }
+
+        sptSet[u] = true;
+
+        for (ADJNODE *adj = g->nodes[u]->neighbor; adj != NULL; adj = adj->next) {
+            int v = adj->nid;
+
+            if (!sptSet[v] && adj->weight && dist[u] != INT_MAX && dist[u] + adj->weight < dist[v]) {
+                dist[v] = dist[u] + adj->weight;
+            }
+        }
+    }
 }
+
 
 EDGELIST *sp_dijkstra(GRAPH *g, int start, int end) {
 // your code
